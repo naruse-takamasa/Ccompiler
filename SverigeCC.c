6faf6e5192773888471956dc0887a8dc1eb6123f
takamasa naruse <45251535+naruse-takamasa@users.noreply.github.com>
2020-03-20 05:38:19 +0900

    数式をコンパイルできた

--- SverigeCC.c
+++ SverigeCC.c
@@ -1,3 +1,14 @@
+/**
+ * @file SverigeCC.c
+ * @author Takamasa Naruse (ckv14093@ict.nitech.ac.jp)
+ * @brief C compiler
+ * @version 0.1
+ * @date 2020-03-20
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -7,6 +18,13 @@
 
 char *user_input;
 
+/**
+ * @brief 文のどこでコンパイルエラーしたかをエラー出力
+ * 
+ * @param loc 
+ * @param fmt 
+ * @param ... 
+ */
 void error_at(char *loc, char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
@@ -20,6 +38,10 @@ void error_at(char *loc, char *fmt, ...) {
 	return;
 }
 
+/**
+ * @brief トークンの種類の定義
+ * 
+ */
 typedef enum {
 	TK_NUM,
 	TK_RESERVED,
@@ -67,6 +89,10 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 	return tok;
 }
 
+bool is_reserved(char *p) {
+	return *p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')';
+}
+
 Token *tokenize(char *p) {
 	Token head;
 	head.next = NULL;
@@ -76,7 +102,7 @@ Token *tokenize(char *p) {
 			p++;
 			continue;
 		}
-		if (*p == '+' || *p == '-') {
+		if (is_reserved(p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -91,6 +117,117 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+/**
+ * @brief 抽象構文木の頂点の定義
+ * @param lhs 左の子ノード
+ * @param rhs 右の子ノード
+ * 
+ */
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+
+Node *primary() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_node_num(expect_num());
+}
+
+Node *unary() {
+	if (consume('+')) {
+		Node *node = primary();
+		return node;
+	} else if (consume('-')) {
+		Node *node = new_node(ND_SUB, new_node_num(0), primary());
+		return node;
+	} else return primary();
+}
+
+Node *mul() {
+	Node *node = unary();
+	for (;;) {
+		if (consume('*')) node = new_node(ND_MUL, node, unary());
+		else if (consume('/')) node = new_node(ND_DIV, node, unary());
+		else return node;
+	}
+}
+
+Node *expr() {
+	Node *node = mul();
+	for (;;) {
+		if (consume('+')) node = new_node(ND_ADD, node, mul());
+		else if (consume('-')) node = new_node(ND_SUB, node, mul());
+		else return node;
+	}
+}
+/**
+ * @brief 抽象構文木からアセンブリを生成
+ * 
+ * @param node 
+ */
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+	gen(node->lhs);
+	gen(node->rhs);
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+	switch (node->kind)
+	{
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	default:
+		break;
+	}
+	printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
@@ -100,21 +237,14 @@ int main(int argc, char **argv) {
 	  fprintf(stderr, "%s\n", argv[1]);
   }
 
-  char *p = argv[1];
   user_input = argv[1];
-  token = tokenize(p);
+  token = tokenize(user_input);
+  Node *node = expr();
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", expect_num());
-  while (!at_eof()) {
-	  if (consume('+')) {
-		  printf("  add rax, %d\n", expect_num());
-		  continue;
-	  }
-	  expect('-');
-	  printf("  sub rax, %d\n", expect_num());
-  }
+  gen(node);
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
